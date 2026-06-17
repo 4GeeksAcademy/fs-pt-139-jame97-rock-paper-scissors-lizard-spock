b9cf448719846cb6516446674e2aebe5c42af848
Jesus Echenagucia <jaem0699@gmail.com>
2026-06-17 09:24:09 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+const juego = (userChoise) => {
+    const opcionUsuario = userChoise;
+    const choises = ["rock", "paper", "scissors", "lizard", "spock"];
+    
+    
+    const randomIndex = Math.floor(Math.random() * choises.length);
+    const opcionComputadora = choises[randomIndex];
 
+
+    const rules = {
+        rock: ["scissors", "lizard"],
+        paper: ["rock", "spock"],
+        scissors: ["paper", "lizard"],
+        lizard: ["paper", "spock"],
+        spock: ["scissors", "rock"]
+    };
+
+
+    if (opcionUsuario === opcionComputadora) {
+        return `User choice: ${opcionUsuario}, Sheldon choice: ${opcionComputadora} -> It's a tie! (Sheldon thinks you cheated)`;
+    }
+
+    if (rules[opcionUsuario].includes(opcionComputadora)) {
+        return `User choice: ${opcionUsuario}, Sheldon choice: ${opcionComputadora} -> You win! (Sheldon cry and thinks you cheated)`;
+    }
+
+    return `User choice: ${opcionUsuario}, Sheldon choice: ${opcionComputadora} -> Sheldon wins, Bazinga!`;
+};
+
+console.log(juego("paper"));
